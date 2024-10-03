f34bed116076023a5b7174725cfd3e1a05371857
Eduardo Dias <165398613+blackeduard@users.noreply.github.com>
2024-10-03 16:17:22 -0300

    att15.Projeto

--- js/scroll.js
+++ js/scroll.js
@@ -12,3 +12,22 @@ window.addEventListener('scroll', () => {
     scroll.classList.remove('fixed')
   }
 })
+
+const navbar = document.getElementById('nav');
+const sections = document.querySelectorAll('ul');
+const navLinks = navbar.querySelectorAll('a');
+
+window.addEventListener('scroll', () => {
+const scrollPosition = window.scrollY + 500; // adjust the offset as needed
+sections.forEach((section) => {
+    if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetTop + section.offsetHeight) {
+    const id = section.getAttribute('id')
+    navLinks.forEach((link) => {
+        link.classList.remove('active');
+        if (link.getAttribute('href') === `#${id}`) {
+        link.classList.add('active')
+        }
+    })
+    }
+})
+})
