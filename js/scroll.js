97ea57eafa6e33a3ea035bb07373dc09cc3aae3f
Eduardo Dias <165398613+blackeduard@users.noreply.github.com>
2024-10-04 14:40:14 -0300

    att18.Projeto

--- js/scroll.js
+++ js/scroll.js
@@ -13,22 +13,26 @@ window.addEventListener('scroll', () => {
   }
 })
 
-const sections = document.querySelectorAll('#nav ul li');
-const navbar = document.getElementById('nav')
-const navLinks = navbar.querySelectorAll('a')
+// Seleciona todos os links dentro da navegação
+const navLinks = document.querySelectorAll('#nav a');
 
-
-window.addEventListener('scroll', () => {
-  const scrollPosition = window.scrollY + 500
-  sections.forEach((section) => {
-      if (scrollPosition >= section.offsetTop && scrollPosition < section.offsetTop + section.offsetHeight) {
-      const id = section.getAttribute('id')
-      navLinks.forEach((link) => {
-          link.classList.remove('active');
-          if (link.getAttribute('href') === `#${id}`) {
-          link.classList.add('active')
-          }
-      })
-      }
+navLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    // Remove a classe 'active' de todos os links
+    navLinks.forEach((l) => {
+      l.classList.remove('active')
+    })
+    // Adiciona a classe 'active' ao link atual
+    link.classList.add('active')
+    // Faz a página scroll para a seção correspondente
+    const href = link.getAttribute('href')
+    const section = document.querySelector(href)
+    section.scrollIntoView({ behavior: 'smooth' })
   })
 })
+// Adiciona evento de click para remover classe active quando clica fora dos links
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('a')) {
+    navLinks.forEach((link) => link.classList.remove('active'));
+  }
+});
