ac7bf95daf7192e8c0e5db31ef0399af86051a81
Eduardo Dias <165398613+blackeduard@users.noreply.github.com>
2024-10-21 15:55:12 -0300

    python

---
+++ main.py
@@ -0,0 +1,12 @@
+frase = list(input('Digite uma frase: ').lower())
+num = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '0']
+alf = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+comp = []
+i = 0
+while True:
+    comp.append(num[i])
+    i = i + 1
+    if i == 5:
+        break
+print(comp)
+print(frase)
